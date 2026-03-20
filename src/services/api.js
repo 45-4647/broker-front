947897aba049715e519b05534e9ac8be8b4032fe
besufikad kasahun <besufikadkasahun12@gmail.com>
2026-03-20 01:01:54 -0700

    edit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: `http://localhost:5000/api` });
+const API = axios.create({ baseURL: `https://broker-back.onrender.com/api` });
 export default API;
 
 
